aca53c31987a8fd23d14b55f414f6f2c48139ebc
korombus <korombus@gmail.com>
2015-12-09 02:48:53 +0900

    adj playerBullet
    playerBullet balance adjustment

--- Scripts/Game/Body/Player/Bullet/PlayerShellBullet.cs
+++ Scripts/Game/Body/Player/Bullet/PlayerShellBullet.cs
@@ -4,6 +4,8 @@ using System;
 
 public class PlayerShellBullet : IPlayerBulletCommand {
 
+    private Vector3 initPos = new Vector3();
+
     public override void Start() {
         base.Start();
         Init();
@@ -16,6 +18,7 @@ public class PlayerShellBullet : IPlayerBulletCommand {
     public override void Init() {
         Damage = 10f;
         Speed = 6f;
+        initPos.Set(this.transform.localPosition.x, this.transform.localPosition.y + 0.33f, this.transform.localPosition.z);
         BulletParent = this.transform.parent.GetComponent<PlayerBulletBase>();
     }
 
@@ -27,7 +30,15 @@ public class PlayerShellBullet : IPlayerBulletCommand {
         if (BulletParent.GetUnenableTag(col2.gameObject.tag)) {
             this.gameObject.SetActive(false);
             this.transform.localPosition = BulletParent.gameObject.transform.localPosition;
+            this.transform.localPosition = initPos;
             BulletParent.SetStopBullet();
+        }
+
+        if(col2.gameObject.tag == "EnemyBullet") {
+            this.gameObject.SetActive(false);
+            this.transform.localPosition = BulletParent.gameObject.transform.localPosition;
+            BulletParent.SetStopBullet();
+            col2.gameObject.SetActive(false);
         }
     }
 }
--- Scripts/Game/Body/Player/Bullet/PlayerWideWayBullet.cs
+++ Scripts/Game/Body/Player/Bullet/PlayerWideWayBullet.cs
@@ -14,7 +14,7 @@ public class PlayerWideWayBullet : IPlayerBulletCommand {
     }
 
     public override void Init() {
-        Damage = 3f;
+        Damage = 1f;
         Speed = 12f;
         BulletParent = this.transform.parent.GetComponent<PlayerBulletBase>();
     }
@@ -24,8 +24,6 @@ public class PlayerWideWayBullet : IPlayerBulletCommand {
     }
 
     public override void OnTriggerEnter2D(Collider2D col2) {
-        Debug.Log(col2.gameObject.tag);
-        Debug.Log(BulletParent.GetUnenableTag(col2.gameObject.tag));
         if (BulletParent.GetUnenableTag(col2.gameObject.tag)) {
             this.gameObject.SetActive(false);
             this.transform.localPosition = BulletParent.gameObject.transform.localPosition;
